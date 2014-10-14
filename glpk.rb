88de765fb36e9bcc056cd2f3cff8a0bcd902a97f
Michael C. Grant <mcg@cvxr.com>
2014-10-14 11:53:52 -0500

    glpk.rb: update to version 4.55

--- glpk.rb
+++ glpk.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Glpk < Formula
   homepage 'http://www.gnu.org/software/glpk/'
-  url 'http://ftpmirror.gnu.org/glpk/glpk-4.52.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/glpk/glpk-4.52.tar.gz'
-  sha1 '44b30b0de777a0a07e00615ac6791af180ff4d2c'
-
+  url 'http://ftpmirror.gnu.org/glpk/glpk-4.55.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/glpk/glpk-4.55.tar.gz'
+  sha1 '893058aada022a8dfc63c675ebcd7e7e86a3a363'
+  
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
